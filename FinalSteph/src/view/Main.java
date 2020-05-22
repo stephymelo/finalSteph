6f1897661c015f8fcb0113d9c2519cf2bad41655
stephym <60659457+stephymelo@users.noreply.github.com>
2020-05-22 13:07:27 -0500

    tenerloListo

---
+++ FinalSteph/src/view/Main.java
@@ -0,0 +1,45 @@
+package view;
+
+import controller.ControllerMain;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	
+
+	
+	
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+	}
+
+	public void settings() {
+		size(800,600);
+	}
+
+	public void setup() {
+		
+
+		
+	}
+
+	public void draw() {
+		background(0);
+		
+	
+	   
+   
+	
+	}
+	
+
+	
+	
+	public void mousePressed() {
+		
+	
+		
+		
+
+	}
+}
+	
